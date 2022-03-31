0f6701205a5c446a26bb6902764147026c08287c
z01241 <z01241@lawbroker.com.tw>
2022-03-31 10:54:46 +0800

    initial

---
+++ lib/lp_csv_exportable_with_stream.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lp_csv_exportable_with_stream/version"
+
+module LpCsvExportableWithStream
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/lp_csv_exportable_with_stream/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LpCsvExportableWithStream
+  VERSION = "0.1.0"
+end
